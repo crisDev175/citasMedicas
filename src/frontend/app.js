6def69437ecdf84f2037d8c0a7d3ce81e7f18954
crisdev <143648868+crisDev175@users.noreply.github.com>
2024-05-21 16:49:30 -0600

    second commit

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,1 +1,1 @@
-alert('works')
+require('./styles/styles.css')
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,47 @@
 const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const devMod = process.env.NODE_ENV !=='production';
 
 module.exports = {
-    entry:'./frontend/app.js',
+    entry: './src/frontend/app.js',
     output: {
-        path: path.join(__dirname, 'backend/public'),
-        filename: 'bundle.js'
-    }
+        path: path.join(__dirname, 'src/backend/public'),
+        filename: 'js/bundle.js',
+    },
+    // cargar codigo css a webpack
+    module: {
+        rules: [
+            { 
+                test: /\.css/, 
+                use: [
+                    devMod ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader'                
+                ]
+            }
+        ],
+    },
+
+    mode: "development",
+    // cargar codigo html a webpack
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/frontend/index.html',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/bundle.css'
+        })
+    ],
+    
+
 }
