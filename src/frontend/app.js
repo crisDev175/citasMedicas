a570da0e470c313ec078952920292806e1a038de
crisdev <143648868+crisDev175@users.noreply.github.com>
2024-05-23 16:31:28 -0600

    creating sever, form some styles frontend

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,1 +1,12 @@
 require('./styles/styles.css')
+
+document.getElementById('form-customer')
+.addEventListener('submit', e =>{
+    const nameCustomer = document.getElementById('customer').value;
+    const ageCustomer = document.getElementById('customer-age').value;
+    const phoneCustomer = document.getElementById('customer-phone').value;
+
+    console.log(nameCustomer, ageCustomer, phoneCustomer);
+    e.preventDefault();
+
+})
---
+++ src/frontend/services/CustomerServices.js
@@ -0,0 +1,6 @@
+class CustomerServices {
+
+    constructor(){
+        this.URI = 'http://localhost:4000/api/v1/customer'
+    }
+}
